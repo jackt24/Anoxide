3713ba1b3c578ec5c96f0e7597e2c0fc1ee85468
jackt24 <48061691+jackt24@users.noreply.github.com>
2021-06-17 10:45:06 +0100

    Addded bg image

--- anoxide_app/app.py
+++ anoxide_app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+# app = Flask(static_folder="C:\Users\Lana\Desktop\Anoxide\anoxide.github.io\anoxide_app\static")
+
 
 
 @app.route('/')
